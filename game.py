d5dd3299ea7faf49fd5dd2967ef388e643a1dd77
Prason <prasonkc07.com>
2023-10-18 13:58:43 +0545

    Added Music and Finished

--- game.py
+++ game.py
@@ -16,14 +16,28 @@ player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 hammar = pygame.image.load("./Resources/hammar_up.png").convert_alpha()
 
+# Initialize the music
+pygame.mixer.init()
+pygame.mixer.music.load("./Audio/bgm.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
+
 score = 0
-dt = 0
+dt = 5
 
 col_detected = False
 
 # Spawn the bug randomly
-bug_x = random.randint(30, 670)
-bug_y = random.randint(30, 670)
+bug_x = random.randint(30, 1200)
+bug_y = random.randint(30, 300)
+
+# Initialize sound effects
+game_start_sound = pygame.mixer.Sound("./Audio/game-start.mp3")
+game_over_sound = pygame.mixer.Sound("./Audio/game-over.wav")
+hit_sound = pygame.mixer.Sound("./Audio/hit.mp3")
+game_over_sound.set_volume(0.5)
+game_over_played = False
+
 
 running = True
 
@@ -32,7 +46,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # Load Background image
     screen.fill("black")
+    background_image = pygame.image.load("./Resources/background.png")
+    bg_width = 1280
+    bg_height = 720
+
+    background_image = pygame.transform.scale(background_image, (bg_width, bg_height))
+    screen.blit(background_image, (0, 0))
     
     # Load the image in the game
     img = pygame.image.load("./Resources/bug.png").convert_alpha()
@@ -42,13 +63,13 @@ while running:
     # Programming the controls
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        player_pos.y -= 300 * dt
+        player_pos.y -= dt
     if keys[pygame.K_DOWN]:
-        player_pos.y += 300 * dt
+        player_pos.y += dt
     if keys[pygame.K_LEFT]:
-        player_pos.x -= 300 * dt
+        player_pos.x -= dt
     if keys[pygame.K_RIGHT]:
-        player_pos.x += 300 * dt
+        player_pos.x += dt
     if keys[pygame.K_SPACE]:
         if not col_detected:
             distance = math.sqrt((player_pos.x - (bug_x + img.get_width() // 2))**2 + (player_pos.y - (bug_y + img.get_height() // 2))**2)
@@ -56,12 +77,14 @@ while running:
             if distance < 40 + max(img.get_width(), img.get_height()) / 2:
                 col_detected = True
                 score += 1
+                dt += 0.2
 
                 # Reset the position of the bug 
-                bug_x = random.randint(30, 670)
-                bug_y = random.randint(30, 670)
+                bug_x = random.randint(30, 1200)
+                bug_y = random.randint(30, 300)
                 
                 hammar = pygame.image.load("./Resources/hammar_down.png").convert_alpha()
+                hit_sound.play()
     else:
         hammar = pygame.image.load("./Resources/hammar_up.png").convert_alpha()
 
@@ -88,8 +111,14 @@ while running:
     bug_displacement = score/10
     bug_y += bug_displacement
     if bug_y > 670:
+        pygame.mixer.music.stop()
         game_over_text = font.render(f'GAME OVER!', True, (255, 255, 255))
         screen.blit(game_over_text, (500, 360))
+
+        if not game_over_played:
+            game_over_sound.play()
+            game_over_played = True
+
 
         # Rectangle parameters
         x, y = 480, 410  # Position of the top-left corner of the rectangle
@@ -103,12 +132,14 @@ while running:
         if keys[pygame.K_SPACE]:
             distance = math.sqrt((player_pos.x - (x + width // 2))**2 + (player_pos.y - (y + height// 2))**2)
             if distance < 40 + max(width, height) / 2:
-                color = (255, 0, 0)
+                game_over_played = False
+                pygame.mixer.music.play(-1)
                 score = 0
+                dt = 5
 
                 screen.fill("black")
-                bug_x = random.randint(30, 670)
-                bug_y = random.randint(30, 670)
+                bug_x = random.randint(30, 500)
+                bug_y = random.randint(30, 500)
                 continue
         
 
@@ -122,9 +153,7 @@ while running:
     pygame.display.flip()
 
     # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+    clock.tick(60)
 
 pygame.quit()
 
